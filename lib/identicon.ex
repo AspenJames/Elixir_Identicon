35363555c3ca122fbd9a88d3605c94662972f987
Aspen James <aspenjames@tqca.org>
2018-11-09 15:53:44 -0800

    add hash_input method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,24 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Generates an Identicon based on a passed-in string
   """
+
+  def main(input) do
+    input
+    |> hash_input
+  end
 
   @doc """
-  Hello world.
+  Converts the passed-in string to a list of numbers based on the MD5 hash of the string
 
-  ## Examples
+  ## Examples 
 
-      iex> Identicon.hello
-      :world
-
+      iex> Identicon.hash_input "identicon"
+      [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
+      
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
