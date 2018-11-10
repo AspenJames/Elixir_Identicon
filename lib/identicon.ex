1be7739b660ae1ea9abb090189bcde003d5ce444
Aspen James <aspenjames@tqca.org>
2018-11-09 16:03:04 -0800

    define Identicon.Image struct, store hashed input in hex key

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,16 +9,20 @@ defmodule Identicon do
   end
 
   @doc """
-  Converts the passed-in string to a list of numbers based on the MD5 hash of the string
+  Converts the passed-in string to a list of numbers based on the MD5 hash of the string, then stores that in an `%Identicon.Image{}` struct, setting that list as the `hex` value
 
   ## Examples 
 
       iex> Identicon.hash_input "identicon"
-      [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
+      %Identicon.Image{
+        hex: [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
+      }
       
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
