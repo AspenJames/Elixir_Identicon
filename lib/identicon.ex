74be33a01b2e5abbe812f17d7aa811da7e0405a2
Aspen James <aspenjames@tqca.org>
2018-11-09 16:13:16 -0800

    Add pick color method, add to struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   @doc """
@@ -13,7 +14,7 @@ defmodule Identicon do
 
   ## Examples 
 
-      iex> Identicon.hash_input "identicon"
+      iex> Identicon.hash_input("identicon")
       %Identicon.Image{
         hex: [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
       }
@@ -24,5 +25,22 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+  Takes in an %Identicon.Image{} and returns the first three elements of the hex list property, setting those as a color property
+
+  ## Examples 
+
+      iex> image = Identicon.hash_input("identicon")
+      iex> Identicon.pick_color(image)
+      %Identicon.Image{
+             color: {173, 43, 65},
+             hex: [173, 43, 65, 97, 60, 135, 2, 181, 55, 43, 189, 201, 168, 16, 112, 64]
+           }
+
+  """
+  def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
