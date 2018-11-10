7272cd8cda8187f05901579ff9d2e992d8f9e8d6
Aspen James <aspenjames@tqca.org>
2018-11-09 16:36:37 -0800

    add mirror_row method, begin build_grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   @doc """
@@ -42,5 +43,25 @@ defmodule Identicon do
   """
   def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    # |> mirror_row
+  end
+
+  @doc """
+  "Mirrors" a list of elements across the center. 
+
+  ## Examples 
+
+      iex> Identicon.mirror_row([173, 43, 65])
+      [173, 43, 65, 43, 173]
+
+  """
+  def mirror_row(row) do
+    [first, second | _] = row
+    row ++ [second, first]
   end
 end
